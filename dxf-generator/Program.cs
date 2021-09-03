5820c6d4e6a1da558005beae81df6a26dcad57ed
Matthias Rink <matthias.rink@innolizer.de>
2021-09-03 11:05:55 +0200

    cleanup of code

--- dxf-generator/Program.cs
+++ dxf-generator/Program.cs
@@ -30,12 +30,9 @@ namespace dxf_genarator
             {
                 for (int y = rasterSize / 2; y < img.Height; y += rasterSize)
                 {
-                    // int pixelvalue = logo.pixels[x + y * logo.width];
                     var pixel = img.GetPixel(x, y);
                     var pixelbrightness = pixel.GetBrightness() * 255.0f;
                     var diameter = Map(pixelbrightness, slope, 255, 0);
-                    // float value = map(pixelbrightness, 255, -50, 0, rasterSize);
-                    // ellipse(x, y, value, value);
                     if (diameter > 0)
                     {
                         var circle = new Circle { Center = new Vector3(x, y, 0), Radius = diameter / 2 };
@@ -45,14 +42,6 @@ namespace dxf_genarator
             }
 
             doc.Save(file);
-
-            // for (int i = 0; i < img.Width; i++)
-            // {
-            //     for (int j = 0; j < img.Height; j++)
-            //     {
-
-            //     }
-            // }
         }
 
         private static float
